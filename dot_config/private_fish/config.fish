ee492f74b9087225ba87d795a04de41ba88be56c
ginloy <liyongleow@gmail.com>
2025-05-28 21:36:51 +0800

    Update .config/fish/config.fish

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -5,7 +5,7 @@ end
 set EDITOR nvim
 set -x ELECTRON_OZONE_PLATFORM_HINT auto
 fish_add_path "$HOME/.local/bin"
-source "$HOME/.cargo/env.fish"
+fish_add_path "$HOME/.cargo/bin"
 alias lg="lazygit"
 zoxide init fish | source
 fastfetch
