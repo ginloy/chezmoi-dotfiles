62d6c063aa2686fbb77a44b7f4484f2908be9d71
ginloy <liyongleow@gmail.com>
2025-06-04 16:00:16 +0800

    set colors for all terminals not just kitty

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -3,6 +3,10 @@ if status is-interactive
     starship init fish | source
 end
 
+if test -f ~/.cache/matugen/sequences
+    cat ~/.cache/matugen/sequences | tr -d '[:space:]' | cat
+end
+
 set EDITOR nvim
 set -x ELECTRON_OZONE_PLATFORM_HINT auto
 fish_add_path "$HOME/.local/bin"
