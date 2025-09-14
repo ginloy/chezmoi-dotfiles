ab8c555ba5194c634a9affbeef9a32dde22dc936
ginloy <liyongleow@gmail.com>
2025-09-14 13:23:26 +0800

    Upate neovim config

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -2,12 +2,10 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-if vim.fn.has("win32") then
-  LazyVim.terminal.setup("pwsh")
-end
-
 local uv = vim.loop
+
 if uv.os_uname().sysname == "Windows_NT" then
+  LazyVim.terminal.setup("pwsh")
   local home = uv.os_getenv("USERPROFILE") or uv.os_getenv("HOMEPATH")
   if home then
     uv.os_setenv("HOME", home)
